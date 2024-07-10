55f3e85196d753c58f38a78c291ce503cd404181
lohug1 <arturo.tapia.r.95@gmail.com>
2024-07-10 02:37:10 -0600

    shortestpath

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ import ColorDetection from './Color-detection/ColorDetection'
 import ShortestPath from './ShortestPath/ShortestPath'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 function App() {
+
   const [count, setCount] = useState(0)
 
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ColorDetection />}>
-          <Route index element={<ColorDetection />} />
-          <Route path="shortest_path" element={<ShortestPath />} />
-          <Route path="*" element={<ColorDetection />} />
-        </Route>
+        <Route path="/" element={<ColorDetection />} />
+        <Route path="shortest_path" element={<ShortestPath />} />
+        <Route path="*" element={<ColorDetection />} />
       </Routes>
     </BrowserRouter>
     </>
